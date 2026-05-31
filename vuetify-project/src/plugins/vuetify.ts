d8f25ef754abbde57753822429dc4419535af6d9
Hannes Morgenthaler <hmo@ingtes.ch>
2026-05-31 15:58:27 +0200

    new vuetify-4 project.

---
+++ vuetify-project/src/plugins/vuetify.ts
@@ -0,0 +1,26 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Composables
+import { createVuetify } from 'vuetify'
+import { forVuetify } from '../theme/breakpoints'
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import '../styles/layers.css'
+
+import 'vuetify/styles'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    defaultTheme: 'system',
+  },
+  display: {
+    mobileBreakpoint: 'md',
+    thresholds: forVuetify,
+  },
+})
